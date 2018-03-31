450d0016f697095d08360cf70ba240a7a12cdba6
Esteve Fernandez <esteve@apache.org>
2018-03-31 11:12:14 +0200

    Update FindTinyXML2.cmake

--- cmake/Modules/FindTinyXML2.cmake
+++ cmake/Modules/FindTinyXML2.cmake
@@ -9,7 +9,7 @@ if(TinyXML2_FOUND)
   if(NOT TINYXML2_LIBRARY AND TARGET tinyxml2)
     # in this case, we're probably using TinyXML2 version 5.0.0 or greater
     # in which case tinyxml2 is an exported target and we should use that
-    set(TINYXML2_LIBRARY tinyxml2)
+    set(TINYXML2_LIBRARY tinyxml2-test1)
   endif()
 else()
   find_path(TINYXML2_INCLUDE_DIR NAMES tinyxml2.h)
